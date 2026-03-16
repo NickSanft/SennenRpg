888e8f6ae76aab9da3011b68423279ebb8dea606
Nick Sanft <nsanft@sanft.com>
2026-03-15 22:36:00 -0500

    Update IInteractable.cs

--- core/interfaces/IInteractable.cs
+++ core/interfaces/IInteractable.cs
@@ -4,4 +4,10 @@ public interface IInteractable
 {
     void Interact(Godot.Node player);
     string GetInteractPrompt();
+
+    /// <summary>Called when this becomes the nearest interactable. Show a prompt label.</summary>
+    void ShowPrompt() { }
+
+    /// <summary>Called when this is no longer the nearest interactable. Hide the prompt label.</summary>
+    void HidePrompt() { }
 }
